0c88e853273a12bad27d2d085a969116cd8c9b6a
vaishnaviaddaguduru <36448226+vaishnaviaddaguduru@users.noreply.github.com>
2018-02-20 21:36:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res) {
 app.get('/article-two', function (req, res) {
   res.send('this is second artilce');
 });
+app.get('/article-one', function (req, res) {
+  res.send('this is first artilce');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
