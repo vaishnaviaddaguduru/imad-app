f53ec25bdc6210572cb2fff93429202b20a55a0f
vaishnaviaddaguduru <36448226+vaishnaviaddaguduru@users.noreply.github.com>
2018-02-22 14:18:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+//change the text of main-text
 var element=document.getElementById('main-text');
 element.innerHTML='my second new value';
+//move the image
+var img=document.getElementById('vaishu');
+img.onclick=function(){
+ img.style.marginleft='100px';   
+};
